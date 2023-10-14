49b3626184cab76b1255bb2ad9dd6b1f2b7ac514
otyacazyu <vesperia.not.xperia@gmail.com>
2023-10-14 13:52:34 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/com/otya/namemanagement/NamemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.otya.namemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamemanagementApplication.class, args);
+	}
+
+}
