c788f42a3d255a06e69280736dcf479b82a9d3b1
otyacazyu <vesperia.not.xperia@gmail.com>
2023-10-15 17:42:56 +0900

    再々修正

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -8,3 +8,4 @@ CREATE TABLE names (
 
 INSERT INTO names (id, name) VALUES (1, "フランク・ダラボン");
 INSERT INTO names (id, name) VALUES (2, "片渕須直");
+INSERT INTO names (id, name) VALUES (3, "宮崎駿");
--- src/main/java/com/otya/namemanagement/NameController.java
+++ src/main/java/com/otya/namemanagement/NameController.java
@@ -1,6 +1,6 @@
 package com.otya.namemanagement;
 
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -8,8 +8,10 @@ import java.util.List;
 @RestController
 public class NameController {
 
-    @Mapping("/names")
+    @GetMapping("/names")
     public List<Name> getNames(){
-        return List.of(new Name(1,"koyama"),new Name(2,"koyama2"));
+        return List.of(new Name(1,"koyama"),new Name(2,"tanaka"),new Name(3,"otya")
+                      ,new Name(4,"imai"));
+
     }
 }
